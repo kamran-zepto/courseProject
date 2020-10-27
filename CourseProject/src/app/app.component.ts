a365d0c56d2c36e03aec38a49ec0605e4ac006a0
Kamran <kamran.mustafa1991@gmail.com>
2020-10-27 14:14:46 +0500

    demo-app

--- CourseProject/src/app/app.component.ts
+++ CourseProject/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CourseProject';
+  serverElements = [
+    { type: 'server', name: 'Test', content: 'This is test content' },
+    { type: 'blueprint', name: 'Test', content: 'This is test content' },
+  ];
 }
